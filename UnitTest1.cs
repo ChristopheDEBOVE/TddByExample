35338240dad0a583787752f52d7d4d18e5611d25
cdebove <cdebove15@gmail.com>
2021-06-27 11:44:49 +0200

    Green Added FrandMultiplicationTest and Franc duplicated from Dollar

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -7,14 +7,19 @@ namespace TddByExample
     public class UnitTest1
     {
         //todo $5 + 10 CHF = $10 if rate is 2:1
-        //todo $5 * 2 = $10
-        //todo make amount private !!!
-        //todo dollar side-effects?
+        //done $5 * 2 = $10
+        //done make amount private 
+        //done dollar side-effects?
         //todo Money rounding?
-        //todo equals
+        //done equals
         //todo getHashCode
         //todo equals null
         //todo equals object
+        //todo 5 CHF * 2 = 10 CHF !!!
+        //todo dollar/franc duplication
+        //todo Common equals
+        //todo Common times
+        
         
         [Fact]
         public void testMultiplication()
@@ -23,6 +28,15 @@ namespace TddByExample
             
             five.Times(2).Should().Be(new Dollar(10));
             five.Times(3).Should().Be(new Dollar(15));
+        }
+        
+        [Fact]
+        public void testFrancMultiplication()
+        {
+            Franc five = new Franc(5);
+            
+            five.Times(2).Should().Be(new Franc(10));
+            five.Times(3).Should().Be(new Franc(15));
         }
 
         [Fact]
@@ -50,6 +64,26 @@ namespace TddByExample
 
         public override bool Equals(object? obj)
             => (obj as Dollar)._amount == _amount;
+
+        public override string ToString() => _amount.ToString();
+    }
+    
+    public class Franc
+    {
+        public Franc(int amount)
+        {
+            this._amount = amount;
+        }
+
+        private int _amount;
+
+        public Franc Times(int multiplier)
+        {
+            return new Franc(_amount*multiplier);
+        }
+
+        public override bool Equals(object? obj)
+            => (obj as Franc)._amount == _amount;
 
         public override string ToString() => _amount.ToString();
     }
