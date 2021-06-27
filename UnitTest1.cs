7cb4b0a37230e9b7a3ffa6c83f200bedf53034d0
cdebove <cdebove15@gmail.com>
2021-06-27 11:20:46 +0200

    Red Dollar side-effects

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -7,9 +7,9 @@ namespace TddByExample
     public class UnitTest1
     {
         //todo $5 + 10 CHF = $10 if rate is 2:1
-        //todo $5 * 2 = $10
+        //todo $5 * 2 = $10 !!!
         //todo make amount private
-        //todo dollar side-effects?
+        //todo dollar side-effects? !!!
         //todo Money rounding?
         
         [Fact]
@@ -18,6 +18,9 @@ namespace TddByExample
             Dollar five = new Dollar(5);
             five.Times(2);
             five.Amount.Should().Be(10);
+            
+            five.Times(3);
+            five.Amount.Should().Be(15);
         }
     }
     
