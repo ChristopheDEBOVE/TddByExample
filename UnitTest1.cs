b35d35b3d10b3415c6f5e4fefd5bc649fd27a792
cdebove <cdebove15@gmail.com>
2021-06-27 11:09:43 +0200

    Red: Ajout test de la Multiplication pour les Dollars

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,13 +1,20 @@
 using System;
+using FluentAssertions;
 using Xunit;
 
 namespace TddByExample
 {
     public class UnitTest1
     {
+        //todo $5 + 10 CHF = $10 if rate is 2:1
+        //todo $5 * 2 = $10
+        
         [Fact]
-        public void Test1()
+        public void testMultiplication()
         {
+            Dollar five = new Dollar(5);
+            five.Times(2);
+            five.Amount.Should().Be(10);
         }
     }
 }
