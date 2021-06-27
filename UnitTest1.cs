c21896eea9bb7c236c532aaacc98f632b65655ee
cdebove <cdebove15@gmail.com>
2021-06-27 13:56:53 +0200

    Root of All Devil

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -30,35 +30,15 @@ namespace TddByExample
             
             five.Times(2).Should().Be(Money.Dollar(10));
             five.Times(3).Should().Be(Money.Dollar(15));
-        }
-        
-        [Fact]
-        public void testFrancMultiplication()
-        {
-            Money five = Money.Franc(5);
-            
-            five.Times(2).Should().Be(Money.Franc(10));
-            five.Times(3).Should().Be(Money.Franc(15));
-        }
+        } 
 
         [Fact]
         public void testEquality()
         {
             Money.Dollar(5).Should().Be(Money.Dollar(5));
             Money.Dollar(5).Should().NotBe(Money. Dollar(6));
-            
-            Money.Franc(5).Should().Be(Money.Franc(5));
-            Money.Franc(5).Should().NotBe(Money.Franc(6));
-            
             Money.Franc(5).Should().NotBe(Money.Dollar(5));
-        }
-        
-        [Fact]
-        public void testDifferentClassEquality()
-        {
-            new Money(5,"CHF").Should().Be(new Franc(5,"CHF"));
-            
-        }
+        } 
 
         [Fact]
         public void testCurrency()
@@ -85,20 +65,11 @@ namespace TddByExample
             =>    (obj as Money).currency == currency
                && (obj as Money).amount == amount;
 
-        public static Money Dollar(int amount)=>new Dollar(amount: amount,"$");
-        public static Money Franc(int amount)=>new Franc(amount: amount,"CHF");
+        public static Money Dollar(int amount)=> new Money(amount: amount,"$");
+        public static Money Franc(int amount)=> new Money(amount: amount,"CHF");
         
         public Money Times(int multiplier)=> new (amount*multiplier,currency);
 
     }
 
-    public class Dollar : Money
-    {
-        public Dollar(int amount, string currency) : base(amount,currency){ }
-    }
-    
-    public class Franc : Money
-    {
-        public Franc(int amount, string currency) : base(amount,currency){ }
-    }
 }
